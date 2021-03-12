11c3d529019472b43f4cf2307a743d220b89823d
Ilya Denezhkin <Denezhkinia@ya.ru>
2021-03-12 15:13:47 +0300

    feat: solved the problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	return typeof array !== 'undefined' && array.length > 0 ? array.sort((a, b) => a - b).reverse().pop() : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	return typeof array !== 'undefined' && array.length > 0 ? array.sort((a, b) => a - b).pop() : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	return typeof array !== 'undefined' && array.length > 0 ? array.reduce((a, b) => (a + b)) / array.length : 0;
 }
